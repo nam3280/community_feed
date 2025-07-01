7b7644f1ad57ba5f09f20175afc6782e8d6ca65b
nam3280 <nam3280@naver.com>
2025-07-01 18:57:35 +0900

    ✨: 좋아요/싫어요 기능 추가

--- src/main/java/org/example/domain/post/Post.java
+++ src/main/java/org/example/domain/post/Post.java
@@ -1,5 +1,6 @@
 package org.example.domain.post;
 
+import org.example.common.PositiveIntegerCounter;
 import org.example.domain.post.content.PostContent;
 import org.example.domain.user.User;
 
@@ -7,13 +8,26 @@ public class Post {
     private final Long id;
     private final User author;
     private final PostContent content;
+    private final PositiveIntegerCounter likeCount;
 
-    public Post(Long id, User author, PostContent content) {
+    public Post(Long id, User author, PostContent content, PositiveIntegerCounter likeCount) {
         if(author == null)
             throw new IllegalArgumentException();
 
         this.id = id;
         this.author = author;
         this.content = content;
+        this.likeCount = new PositiveIntegerCounter();
+    }
+
+    public void like(User user){
+        if(this.author.equals(user))
+            throw new IllegalArgumentException();
+
+        likeCount.increase();
+    }
+
+    public void unlike(){
+        likeCount.decrease();
     }
 }
