e3537622aec569888144aa41f805c12431374282
nam3280 <nam3280@naver.com>
2025-07-01 17:34:38 +0900

    ✨: follow, unfollow 기능 및 domain 추가

--- src/main/java/org/example/user/domain/User.java
+++ src/main/java/org/example/user/domain/User.java
@@ -5,10 +5,30 @@ import java.util.Objects;
 public class User {
     private final Long id;
     private final UserInfo info;
+    private final UserRelationCounter followingCounter;
+    private final UserRelationCounter followerCounter;
 
     public User(Long id, UserInfo userInfo) {
         this.id = id;
         this.info = userInfo;
+        this.followingCounter = new UserRelationCounter();
+        this.followerCounter = new UserRelationCounter();
+    }
+
+    public void follow(User targetUser){
+        if(targetUser.equals(this))
+            throw new IllegalArgumentException();
+
+        followingCounter.increase();
+        targetUser.followerCounter.increase();
+    }
+
+    public void unFollow(User targetUser){
+        if(targetUser.equals(this))
+            throw new IllegalArgumentException();
+
+        followingCounter.decrease();
+        targetUser.followerCounter.decrease();
     }
 
     @Override
