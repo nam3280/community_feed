98d43f6a1d062c2691acbb314caab70816469155
nam3280 <nam3280@naver.com>
2025-07-02 23:00:52 +0900

    ✨: 유저 service 생성

---
+++ src/main/java/org/example/user/application/UserService.java
@@ -0,0 +1,20 @@
+package org.example.user.application;
+
+import org.example.user.application.dto.CreateUserRequestDto;
+import org.example.user.application.interfaces.UserRepository;
+import org.example.user.domain.User;
+import org.example.user.domain.UserInfo;
+
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(CreateUserRequestDto dto){
+        UserInfo info = new UserInfo(dto.name(), dto.profileImageUrl());
+        User user = new User(null, info);
+        return userRepository.save(user);
+    }
+}
