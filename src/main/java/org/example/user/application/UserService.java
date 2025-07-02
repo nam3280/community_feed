8697323fa8a3e5b8598108cd8592fc2a76d616f2
nam3280 <nam3280@naver.com>
2025-07-02 23:11:42 +0900

    ✨: getUser 기능 추가

--- src/main/java/org/example/user/application/UserService.java
+++ src/main/java/org/example/user/application/UserService.java
@@ -5,6 +5,8 @@ import org.example.user.application.interfaces.UserRepository;
 import org.example.user.domain.User;
 import org.example.user.domain.UserInfo;
 
+import java.util.IllformedLocaleException;
+
 public class UserService {
     private final UserRepository userRepository;
 
@@ -16,5 +18,9 @@ public class UserService {
         UserInfo info = new UserInfo(dto.name(), dto.profileImageUrl());
         User user = new User(null, info);
         return userRepository.save(user);
+    }
+
+    public User getUser(Long id){
+        return userRepository.findById(id).orElseThrow(IllformedLocaleException::new);
     }
 }
